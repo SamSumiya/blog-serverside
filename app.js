59c0ed895a3f615ebe4dba2ead788b3208b6e89d
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-06 19:54:32 -0800

    connected to mongodb first before listening to ports

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-// load config and put all the global variables here 
-dotenv.config({ path: './config/config.env'})
-const PORT = process.env.PORT || 5050; 
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+// load config and put all the global variables here
+dotenv.config({ path: './config/config.env' });
+const PORT = process.env.PORT || 5050;
 
+const connctDB = require('./db/connectDB');
 
-const app = express(); 
+const app = express();
 
-app.use(morgan('dev'))
-
-
-
-
+app.use(morgan('dev'));
 
 
 
+async function start() {
+  try {
+    const conn = await connctDB(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening in ${process.env.NODE_ENV} mode on port ${PORT}`);
-})
+    app.listen(PORT, () => {
+      console.log(
+        `Server is listening in ${process.env.NODE_ENV} mode on port ${PORT}`
+      );
+    });
+  } catch (error) {}
+}
 
+
+start() 
