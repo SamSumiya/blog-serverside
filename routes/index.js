75657c51eb6f551eaddb28c10c1c8e53e71bd930
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-07 12:08:29 -0800

    added a table section in dashboard

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 const { get_loginPage, get_dashboard } = require('../controller/index');
 
+
 // Login page
 router.get('/login', ensureGuest, get_loginPage);
 router.get('/dashboard', ensureAuth, get_dashboard);
+
+
 
 module.exports = router
