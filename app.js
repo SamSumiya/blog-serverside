d57521231f02c35605dfec1845901237a3867f5b
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-06 20:14:28 -0800

    got handle bars

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars')
+
 // load config and put all the global variables here
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5050;
 
 const connctDB = require('./db/connectDB');
 
+
+
 const app = express();
 
-app.use(morgan('dev'));
+app.set('view engine', 'exphbs');
+
+process.env.NODE_ENV === 'development' ? app.use(morgan('dev')) : null 
 
 
 
@@ -18,7 +24,7 @@ async function start() {
     const conn = await connctDB(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
 
-    app.listen(PORT, () => {
+    await app.listen(PORT, () => {
       console.log(
         `Server is listening in ${process.env.NODE_ENV} mode on port ${PORT}`
       );
